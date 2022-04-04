94740a73d2fc787cdf3b62233e4c7f8fe1b964d3
William Baker <bbrk24@gmail.com>
2022-04-04 09:42:14 -0400

    Change example project to SwiftUI

---
+++ Example/swiftdi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftdi
+//
+//  Created by William Baker on 04/04/2022.
+//  Copyright (c) 2022 Tiny Home Consulting LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example/swiftdi/ExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleApp.swift
+//  swiftdi
+//
+//  Created by William Baker on 04/04/2022.
+//  Copyright (c) 2022 Tiny Home Consulting LLC. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
