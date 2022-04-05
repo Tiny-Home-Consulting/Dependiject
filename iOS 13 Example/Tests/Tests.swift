939ac51c52d7ed34de310c776308e50a524af8ae
bbrk24 <bbrk24@gmail.com>
2022-04-05 16:02:17 -0400

    Scaffold CocoaPod (#1)
    * Scaffold cocoapod
    
    * Add everything to xcodeproj
    
    * Update Podfile
    
    * Update license
    
    * Change example project to SwiftUI
    
    * Separate iOS 13 and iOS 14 examples
    
    * Fixed the scene delegate

---
+++ iOS 13 Example/Tests/Tests.swift
@@ -0,0 +1,37 @@
+//
+//  Tests.swift
+//  swiftdi_Tests
+//
+//  Created by William Baker on 03/31/2022.
+//  Copyright (c) 2022 Tiny Home Consulting LLC. All rights reserved.
+//
+
+import XCTest
+import swiftdi
+@testable import swiftdi_Example
+
+class Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
