c80ecc58a9b96a70d63f54cb24bb7336bf6d5df1
William Baker <bbrk24@gmail.com>
2022-04-11 10:43:20 -0400

    Add tests for iOS 14 example

--- iOS 14 Example/swiftdi_ExampleTests/Tests.swift
+++ iOS 14 Example/swiftdi_ExampleTests/Tests.swift
@@ -7,29 +7,53 @@
 //
 
 import XCTest
+import swiftdi
+import Mockingbird
 @testable import swiftdi_Example
 
-class swiftdi_ExampleTests: XCTestCase {
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class Tests: XCTestCase {
+    var mockFetcher: DataFetcherMock!
+    var mockValidator: DataValidatorMock!
+    var sut: ContentViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        
+        mockFetcher = mock(DataFetcher.self)
+        given(mockFetcher.getData()).willReturn([1, 2, 3, 4, 5, 6])
+        
+        mockValidator = mock(DataValidator.self)
+        given(mockValidator.pickValidItems(from: any())).will { $0 }
+        
+        /*
+         * In this example case, I'll admit that using the factory here isn't entirely necessary,
+         * since the SUT takes its dependencies as arguments to the initializer. However, that isn't
+         * necessarily the case -- for example, the view grabs its own view model from the factory
+         * -- so I'm using the factory here to demonstrate that it is possible.
+         */
+        
+        Factory.register {
+            Service(.transient, DataFetcher.self) { _ in
+                self.mockFetcher
+            }
+            
+            Service(.transient, DataValidator.self) { _ in
+                self.mockValidator
+            }
         }
+        
+        sut = ContentViewModelImplementation(
+            fetcher: Factory.shared.getInstance()!,
+            validator: Factory.shared.getInstance()!
+        )
+    }
+    
+    func testExample() {
+        sut.refreshData()
+        
+        XCTAssertEqual(sut.array, [1, 2, 3, 4, 5, 6])
+        
+        verify(mockFetcher.getData()).wasCalled(1)
+        verify(mockValidator.pickValidItems(from: any())).wasCalled(1)
     }
 }
