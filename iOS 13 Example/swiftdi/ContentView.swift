8a4318ab655d497779ce4c02e7a49cc96b09e5ee
William Baker <bbrk24@gmail.com>
2022-04-11 10:38:21 -0400

    Switch to XUI pod

--- iOS 13 Example/swiftdi/ContentView.swift
+++ iOS 13 Example/swiftdi/ContentView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import XUI
 import swiftdi
 
 struct ContentView: View {
--- iOS 13 Example/swiftdi/ContentViewModel.swift
+++ iOS 13 Example/swiftdi/ContentViewModel.swift
@@ -6,7 +6,7 @@
 //  Copyright (c) 2022 Tiny Home Consulting LLC. All rights reserved.
 //
 
-import Combine
+import XUI
 
 protocol ContentViewModel: AnyObservableObject {
     /// A list of things to display.
