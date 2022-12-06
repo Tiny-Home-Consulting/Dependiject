cc345a7cb840a471a4f0d6e37428fe7229087194
William Baker <bbrk24@gmail.com>
2022-12-06 13:56:27 -0500

    tests and changelog

--- Tests/MultitypeServiceTests.swift
+++ Tests/MultitypeServiceTests.swift
@@ -108,4 +108,61 @@ final class MultitypeServiceTests: BaseFactoryTest {
             )
         }
     }
+    
+    func test_multitypeServiceClosure_sharedNameResolves() {
+        Factory.register {
+            MultitypeService(exposedAs: [P1.self, P2.self], name: "test name") { _ in
+                C()
+            }
+        }
+        
+        let p1Instance = Factory.shared.resolve(P1.self, name: "test name")
+        let p2Instance = Factory.shared.resolve(P2.self, name: "test name")
+        
+        XCTAssertIdentical(p1Instance, p2Instance, "Both protocols should give the same instance")
+    }
+    
+    func test_multitypeServiceConstant_sharedNameResolves() {
+        Factory.register {
+            MultitypeService(exposedAs: [P1.self, P2.self], name: "test name", C())
+        }
+        
+        let p1Instance = Factory.shared.resolve(P1.self, name: "test name")
+        let p2Instance = Factory.shared.resolve(P2.self, name: "test name")
+        
+        XCTAssertIdentical(p1Instance, p2Instance, "Both protocols should give the same instance")
+    }
+    
+    func test_multitypeServiceClosure_individualNamesStaySeparate() {
+        Factory.register {
+            MultitypeService(exposedAs: [
+                P1.self: "protocol 1",
+                P2.self: "protocol 2",
+                C.self: "class"
+            ]) { _ in
+                C()
+            }
+        }
+        
+        _ = Factory.shared.resolve(P1.self, name: "protocol 1")
+        _ = Factory.shared.resolve(P2.self, name: "protocol 2")
+        _ = Factory.shared.resolve(C.self, name: "class")
+    }
+    
+    func test_multitypeServiceConstant_individualNamesStaySeparate() {
+        Factory.register {
+            MultitypeService(
+                exposedAs: [
+                    P1.self: "protocol 1",
+                    P2.self: "protocol 2",
+                    C.self: "class"
+                ],
+                C()
+            )
+        }
+        
+        _ = Factory.shared.resolve(P1.self, name: "protocol 1")
+        _ = Factory.shared.resolve(P2.self, name: "protocol 2")
+        _ = Factory.shared.resolve(C.self, name: "class")
+    }
 }
